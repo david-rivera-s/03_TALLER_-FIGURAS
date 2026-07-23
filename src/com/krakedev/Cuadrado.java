1515bfb7eaeadc140eeb5b3b0abb0eb3a71e4c81
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-22 20:42:58 -0500

    feat(cuadrado): crear clase Cuadrado con atributo lado y metodos calcularArea y calcularPerimetro

---
+++ src/com/krakedev/Cuadrado.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class Cuadrado {
+	
+public double lado;
+    
+    public double calcularArea() {
+        return lado * lado;
+    }
+    
+    public double calcularPerimetro() {
+        return lado * 4;
+    }
+
+}
