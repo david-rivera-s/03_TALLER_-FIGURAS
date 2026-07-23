7481313be16b6f8758bc9e71db8151a5b0892f6c
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-22 20:11:20 -0500

    feat(rectangulo): declarar atributos base y altura y agregar metodo calcularPerimetro

--- src/com/krakedev/Rectangulo.java
+++ src/com/krakedev/Rectangulo.java
@@ -1,5 +1,12 @@
 package com.krakedev;
 
 public class Rectangulo {
+    
+    public double base;
+    public double altura;
+	
+    public double calcularPerimetro() {
+        return (base * 2) + (altura * 2);
+    }
 
 }
